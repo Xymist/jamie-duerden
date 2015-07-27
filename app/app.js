cf08756e59544132f2368c41288d223218797cfb
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-07-27 18:14:07 +0100

    navbuttons

--- app/app.js
+++ app/app.js
@@ -40,4 +40,20 @@ angular.module('myApp', [
     }])
     .controller('OldProjectCtrl', [function () {
 
-    }]);
+    }])
+    .controller('DropdownCtrl', function ($scope, $log) {
+
+        $scope.status = {
+            isopen: false
+        };
+
+        $scope.toggled = function(open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        $scope.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.status.isopen = !$scope.status.isopen;
+        };
+    });
