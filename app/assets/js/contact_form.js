f8d2ebbdd322ef29328a269e72a1866d7109bcc6
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-07-27 22:34:45 +0100

    name flag

--- app/assets/js/contact_form.js
+++ app/assets/js/contact_form.js
@@ -1,15 +1,15 @@
 function encrypt() {
-    if (window.crypto.getRandomValues) {
-        var key = document.getElementById('pub-key').val;
-        var pub_key = openpgp.key.readArmored(key);
-        var pgp_message = openpgp.encryptMessage(pub_key.keys, $('#main-input').val());
-        $('#main-input').val(pgp_message);
-        $('#encrypt-button').style.display="none";
+//    if (window.crypto.getRandomValues) {
+//        var key = document.getElementById('pub-key').val;
+//        var pub_key = openpgp.key.readArmored(key);
+//        var pgp_message = openpgp.encryptMessage(pub_key.keys, $('#main-input').val());
+//        $('#main-input').val(pgp_message);
+//        $('#encrypt-button').style.display="none";
         $("contact-form").submit();
-        return true;
-    } else {
-        $("#encrypt-button").val("Error");
-        window.alert("This browser isn't supported!");
-        return false;
-    }
+//        return true;
+//    } else {
+//        $("#encrypt-button").val("Error");
+//        window.alert("This browser isn't supported!");
+//        return false;
+//    }
 }
