a5e770424bb2174d6029e66bbf137f2a97cc1e6d
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-08-06 19:17:07 +0100

    CV repairs

--- app/app.js
+++ app/app.js
@@ -40,9 +40,24 @@ angular.module('myApp', [
     .controller('ContactCtrl', [function () {
 
     }])
-    .controller('OldProjectCtrl', [function ($scope) {
-
-    }])
+    .controller('OldProjectCtrl', function ($scope) {
+        $scope.skillsList = [
+            "Interviewing suppliers",
+            "Drafting documents and reports",
+            "Data analysis",
+            "Presentation to groups",
+            "Communicating technical information to non-technical external and internal stakeholders",
+            "Stakeholder engagement",
+            "Problem solving and troubleshooting",
+            "Project stream planning",
+            "Teamwork and coordination",
+            "Web construction and administration with HTML5, SCSS, AngularJS, Ruby on Rails",
+            "Detailed taste memory",
+            "Competent with Microsoft Word, Excel, PowerPoint and Publisher",
+            "Competent with the Linux command line interface.",
+            "Change management"
+        ];
+    })
     .controller('DropdownCtrl', function ($scope, $log) {
 
         $scope.status = {
--- app/assets/js/cv-accordion.js
+++ app/assets/js/cv-accordion.js
@@ -50,20 +50,4 @@ angular.module('myApp.cvAccordion', []).controller('CVAccordionCtrl', function (
         }
     ];
 
-    $scope.skillsList = [
-        "Interviewing suppliers",
-        "Drafting documents and reports",
-        "Data analysis",
-        "Presentation to groups",
-        "Communicating technical information to non-technical external and internal stakeholders",
-        "Stakeholder engagement",
-        "Problem solving and troubleshooting",
-        "Project stream planning",
-        "Teamwork and coordination",
-        "Web construction and administration with HTML5, SCSS, AngularJS, Ruby on Rails",
-        "Detailed taste memory",
-        "Competent with Microsoft Word, Excel, PowerPoint and Publisher",
-        "Competent with the Linux command line interface.",
-        "Change management"
-    ];
 });
