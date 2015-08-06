5d3d168b59657eef3fdb2da722a4af8f961aea5c
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-08-06 17:10:44 +0100

    CV initial

--- app/assets/js/cv-accordion.js
+++ app/assets/js/cv-accordion.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp.cvAccordion', []).controller('CVAccordionCtrl', function ($scope) {
-    $scope.groups = [
+    $scope.jobGroups = [
         {
             employer: "Citizens' Advice Waverley",
             title: 'Project Technical Officer',
@@ -9,10 +9,48 @@ angular.module('myApp.cvAccordion', []).controller('CVAccordionCtrl', function (
             details: ''
         },
         {
-            employer: "Citizens' Advice Waverley",
-            title: 'Project Technical Officer',
-            dates: '',
+            employer: "Maid in Sheffield (BRD22OY Ltd)",
+            title: 'Technical Director',
+            dates: 'July 2014-March 2015',
+            details: ''
+        },
+        {
+            employer: "Richard Royds Fine Wines Ltd",
+            title: 'Technical Support',
+            dates: 'June 2010 – Current',
             details: ''
         }
+    ];
+
+    $scope.educationGroups = [
+        {
+            school: "University of Bath",
+            title: 'BSc Chemistry',
+            dates: 'October 2011 - July 2014',
+            details: 'Second Class Honours, Second Division'
+        },
+        {
+            school: "Winchester College",
+            title: 'A Levels',
+            dates: 'September 2008 - July 2010',
+            details: ''
+        }
+    ];
+
+    $scope.skillsList = [
+        "Interviewing suppliers",
+        "Drafting documents and reports",
+        "Data analysis",
+        "Presentation to groups",
+        "Communicating technical information to non-technical external and internal stakeholders",
+        "Stakeholder engagement",
+        "Problem solving and troubleshooting",
+        "Project stream planning",
+        "Teamwork and coordination",
+        "Web construction and administration with HTML5, SCSS, AngularJS, Ruby on Rails",
+        "Detailed taste memory",
+        "Competent with Microsoft Word, Excel, PowerPoint and Publisher",
+        "Competent with the Linux command line interface.",
+        "Change management"
     ];
 });
