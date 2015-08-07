28b4f385c04a9659157f38060de3078f669fef81
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-08-08 00:52:18 +0100

    pre test

--- app/assets/js/cv-accordion.js
+++ app/assets/js/cv-accordion.js
@@ -6,7 +6,7 @@ angular.module('myApp.cvAccordion', []).controller('CVAccordionCtrl', function (
     $scope.jobGroups = [
         {
             employer: "Citizens' Advice Waverley",
-            title: '<h3>Project Technical Officer (January 2015 - Current)</h3>',
+            title: 'Project Technical Officer (January 2015 - Current)',
             details: 'Working to provide VoIP'
         },
         {
