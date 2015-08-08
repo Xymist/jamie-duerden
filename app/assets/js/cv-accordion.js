1e41ef582c7ab52356df06db71e268dcea49d9ba
Jamie Duerden <jamie@jamieduerden.co.uk>
2015-08-08 22:06:40 +0100

    Working CV buttons

--- app/assets/js/cv-accordion.js
+++ app/assets/js/cv-accordion.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp.cvAccordion', []).controller('CVAccordionCtrl', function ($scope) {
-    $scope.radio = {model :  "Experience"};
+    $scope.radio = {model :  undefined};
 
     $scope.jobGroups = [
         {
