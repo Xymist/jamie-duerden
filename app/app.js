c20716409bea2ac348130205b82304721f4bcf64
James Duerden <james@jamieduerden.co.uk>
2015-12-03 23:49:08 +0000

    Shall we see what happens?

--- app/app.js
+++ app/app.js
@@ -17,7 +17,14 @@ angular.module('myApp', [
 
     }])
     .controller('ProjectCtrl', [function ($scope) {
-        $scope.projects = $.getJSON('projects.json');
+        $scope.projects = [
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+            {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"}
+        ];
     }])
     .controller('ContactCtrl', [function () {
 
