0bee47ed0cd8119a1b3970a29e35e65137cdd381
James Duerden <james@jamieduerden.co.uk>
2015-12-04 23:29:37 +0000

    Yay, I broke it again!

--- app/assets/js/projects.js
+++ app/assets/js/projects.js
@@ -1,11 +1,11 @@
 'use strict';
 angular.module('jamieApp.projects', []).controller('ProjectCtrl', function ($scope) {
     $scope.projects = [
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"firstID"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"secondID"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"thirdID"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"fourthID"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"fifthID"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"sixthID"}
+        {"title":"ASTF VoIP", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+        {"title":"ASTF Kiosks", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+        {"title":"Waverley Advice Website", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+        {"title":"Maid in Sheffield", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
+        {"title":"Haslemere Cellar website", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"}
     ]
 });
