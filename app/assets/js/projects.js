4341250dc605bd7642e4ef9678ad482ab064e760
James Duerden <james@jamieduerden.co.uk>
2015-12-04 01:04:18 +0000

    Let's try this...

--- app/assets/js/projects.js
+++ app/assets/js/projects.js
@@ -1,11 +1,11 @@
 'use strict';
 angular.module('jamieApp.projects', []).controller('ProjectCtrl', function ($scope) {
     $scope.projects = [
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"}
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"firstID"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"secondID"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"thirdID"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"fourthID"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"fifthID"},
+        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png", "id":"sixthID"}
     ]
 });
