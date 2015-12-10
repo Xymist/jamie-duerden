2176ffd686f10e63b4f0f6711795e5a61093b226
James Duerden <james@jamieduerden.co.uk>
2015-12-10 20:11:57 +0000

    make it work

--- app/assets/js/projects.js
+++ app/assets/js/projects.js
@@ -4,7 +4,6 @@ angular.module('jamieApp.projects', []).controller('ProjectCtrl', function ($sco
         {"title":"ASTF VoIP", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
         {"title":"ASTF Kiosks", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
         {"title":"Waverley Advice Website", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
-        {"title":"test title", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
         {"title":"Maid in Sheffield", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"},
         {"title":"Haslemere Cellar website", "description":"test description", "imageLink":"/site/app/assets/img/logo.png"}
     ]
