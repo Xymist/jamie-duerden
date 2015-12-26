c1499a070ee2c36b98d05e84c7dd2c308b0792a3
James Duerden <jamie.f.duerden@gmail.com>
2015-12-26 01:30:12 +0000

    Next version of encrypt js

--- app/assets/js/contact_form.js
+++ app/assets/js/contact_form.js
@@ -1,9 +1,9 @@
 function encrypt_if_possible() {
   var openpgp = require('openpgp');
-  var key = document.getElementById('pub-key').val();
+  var key = document.getElementById('pub-key').innerHTML;
   var publicKey = openpgp.key.readArmored(key);
-  openpgp.encryptMessage(publicKey.keys, $('#main-input').val()).then(function(pgpMessage) {
-    $('#main-input').val(pgp_message);
+  openpgp.encryptMessage(publicKey.keys, document.getElementById('main-input').value).then(function(pgpMessage) {
+    $('#main-input').value=pgpMessage;
     $("contact-form").submit();
   }).catch(function(error) {
     console.log("Failed to encrypt message; sending in plaintext");
